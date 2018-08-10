d229e74e69b1fdceb4834b0ed3171600af7eb434
Sav Younts <savyounts@gmail.com>
2018-08-10 22:09:47 +0000

    automatically backed up by learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -1,7 +1,7 @@
 module Dance 
   
   def twirl
-    "I'm twirling"
+    "I'm twirling!"
   end 
   
   def jump
@@ -13,6 +13,6 @@ module Dance
   end
   
   def take_a_bow 
-    "Thank you, thank  you. It was a pleasure to dance for you all."
+    "Thank you, thank you. It was a pleasure to dance for you all."
   end
 end 
